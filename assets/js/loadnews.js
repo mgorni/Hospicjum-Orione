76b3d7eb29472c18d98398a1c737af311eae89f1
mgorni <47871945+mgorni@users.noreply.github.com>
2026-03-07 16:02:05 +0100

    Update loadnews.js

--- assets/js/loadnews.js
+++ assets/js/loadnews.js
@@ -1,4 +1,4 @@
-<script>
+
   alert "Test";
   const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQpj_CYtCwZIaXHYU-WwHK0TM40gMnBXwrmr5PTK0mj9SQ4Edwa-oB_eS48BwxN4sRGf4FurP0dIrJI/pub?gid=0&single=true&output=csv";
   alert  SHEET_CSV_URL;
@@ -169,4 +169,3 @@
   }
 
   document.addEventListener("DOMContentLoaded", loadNews);
-</script>
